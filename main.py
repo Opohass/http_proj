809806b67bab67c78ae9911e43b157ffed8aee44
lior3300 <jacobtherobber@gmail.com>
2022-08-23 13:50:32 +0300

    added requirments file. updated the gitignore

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# blah blah blah
+
+from flask import Flask
+
+app =Flask(__name__)
+
+#index route
+
+@app.route("/")
+def index():
+    return "test"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
