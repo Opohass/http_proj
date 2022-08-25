0d059932273ec66db7709aee6627c9347db67a71
lior3300 <jacobtherobber@gmail.com>
2022-08-25 11:59:57 +0300

    fixed bug in picture upload, added sending the answer to the html page

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ app =Flask(__name__)
 #index route
 @app.route("/")
 def index():
-    return render_template("index.html")
+    answer="CC"
+    return render_template("index.html",answer=answer)
 
+#when image upload route
 @app.route("/upload", methods = ['GET', 'POST'])
 def uploaded():
     if request.method == 'POST':
         f = request.files['file'].read()
-        file_bytes = np.frombuffer(f, np.uint8)
-        final1DImage=imgConv.getImageAsArray(file_bytes)
+        if f != b"":
+            file_bytes = np.frombuffer(f, np.uint8)
+            final1DImage=imgConv.getImageAsArray(file_bytes)
+            #TODO: connect the model, insert the prediction into the HTML
+            answer="i need to implement this..."
+        else:
+            answer="TEST"
 
-        #TODO: connect the model, insert the prediction into the HTML
-        print(final1DImage)
-        return render_template("index.html")
+        return render_template("index.html",answer=answer)
 
 if __name__=="__main__":
     app.run(debug=True)
