664d4c14609652d562637abccc5f0a427fe8b557
lior3300 <jacobtherobber@gmail.com>
2022-08-25 11:42:27 +0300

    changed deprecated method to a new one

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 from flask import Flask, render_template, request
 
 app =Flask(__name__)
-app.config['UPLOAD_FOLDER']="./"
-app.config['MAX_CONTENT_PATH']=100
+#where to save a file if we add this option, now the image uploaded isn't saved
+# app.config['UPLOAD_FOLDER']="./" 
+# app.config['MAX_CONTENT_PATH']=100
 
 #index route
 @app.route("/")
@@ -15,19 +16,12 @@ def index():
 def uploaded():
     if request.method == 'POST':
         f = request.files['file'].read()
-        file_bytes = np.fromstring(f, np.uint8)
+        file_bytes = np.frombuffer(f, np.uint8)
         final1DImage=imgConv.getImageAsArray(file_bytes)
 
         #TODO: connect the model, insert the prediction into the HTML
-        
+        print(final1DImage)
         return render_template("index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
-
-# #read image file string data
-# filestr = request.files['file'].read()
-# #convert string data to numpy array
-# file_bytes = numpy.fromstring(filestr, numpy.uint8)
-# # convert numpy array to image
-# img = cv.imdecode(file_bytes, cv.IMREAD_UNCHANGED)
